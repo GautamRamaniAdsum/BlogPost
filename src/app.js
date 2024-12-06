bc9ece7527f01b7e51ba4e73fedece15fafc4755
GautamRamaniAdsum <101443138+GautamRamani@users.noreply.github.com>
2024-12-06 11:08:54 +0530

    health route change

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(cors())
 app.use(morgan("tiny"))
 
 app.get("/health", (req, res) => {
-    res.status(200).send({ status: "success", msg: "Github CI-CD" })
+    res.status(200).send({ status: "success", msg: "CI-CD SETUP" })
 })
 
 app.use("/api", router)
